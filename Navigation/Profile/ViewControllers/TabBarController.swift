fd1e83de2bde8cf62cdb6d1dfdd2fb212c8f57f7
Yulia Valieva <japanesecase@gmail.com>
2024-03-20 10:25:33 +0300

    HW7

--- Navigation/Profile/ViewControllers/TabBarController.swift
+++ Navigation/Profile/ViewControllers/TabBarController.swift
@@ -6,6 +6,7 @@ class TabBarController: UITabBarController {
     
     var feedNavigationController : UINavigationController!
     var logInNavigationController : UINavigationController!
+    var favoritesPostsController: UINavigationController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,11 +20,13 @@ class TabBarController: UITabBarController {
 //        loginViewController.loginDelegate = loginInspector - задача 1
         feedNavigationController = UINavigationController.init(rootViewController: FeedViewController ())
         logInNavigationController = UINavigationController.init(rootViewController: loginViewController)
+        favoritesPostsController = UINavigationController.init(rootViewController: FavorietsPostsVC())
         
-        self.viewControllers = [feedNavigationController, logInNavigationController]
+        self.viewControllers = [feedNavigationController, logInNavigationController, favoritesPostsController]
         
         feedNavigationController.tabBarItem = UITabBarItem (title: "Feed", image: UIImage(systemName: "heart.text.square"), tag: 0)
         logInNavigationController.tabBarItem = UITabBarItem (title: "Profile", image: UIImage(systemName: "person"), tag: 1)
+        favoritesPostsController.tabBarItem = UITabBarItem (title: "like", image: UIImage(systemName: "heart"), tag: 2)
         
         UITabBar.appearance().tintColor = UIColor (red: 0/105.0, green: 0/105.0, blue: 236/105.0, alpha: 1.0)
         UITabBar.appearance().backgroundColor = .systemGray6
